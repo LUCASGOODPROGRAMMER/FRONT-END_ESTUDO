1163d715e0fc5144060e2f77f59da361611ae247
Lucas Hubner <160272134+LUCASGOODPROGRAMMER@users.noreply.github.com>
2025-06-18 20:43:35 -0400

    métodos de um array, propriedades arrays e laços para arrays

--- html_css_js/05_arrays_e_objs/js/script.js
+++ html_css_js/05_arrays_e_objs/js/script.js
@@ -1,4 +1,5 @@
-// arrays
+// arrays 
+/*
 const lista = [1, 2, 3, 4, 5]; //lista com dados numéricos, lista do tipo object
 console.log(lista);
 console.log(typeof lista);
@@ -77,3 +78,127 @@ console.log(obj2.a)
 console.log(Object.keys(objeto))
 console.log(Object.keys(obj2)) //usado para ver as keys de um objeto, propriedades no caso
 console.log(Object.entries(obj2)) //acha as keys, o tipo, o valor e a posição
+
+// mutação (Mutability)
+const b = {
+    name: 'lucas'
+}
+
+const a = b // ao definir um variavel igual um objeto, ambos serão influenciado pelos comandos aplicado a variavel ou objeto
+// ele não cria uma cópia mas sim uma referência, um atalho para modificar o objeto b
+
+console.log(a)
+console.log(b)
+console.log(a === b)
+console.log(b === a)
+
+b.age = 17
+console.log(b)
+console.log(a)
+
+delete a.age
+
+console.log(b)
+console.log(a)
+*/
+
+// Loops em arrays
+const usuarios = ['lucas', 'keynner', 'einstein', 'kaylla', 'tainá']
+
+for(let i = 0; i < usuarios.length; i++) //length conta quantas posições tem o array, ou valores, ou caracteres. depende do tipo da variável
+{
+    console.log(`usuário n°${i + 1} ${usuarios[i]} na posição ${i}`)
+}
+
+// Métodos de array:push e pop
+// push usado para adicionar algo no fim do array
+// pop remove um elemento no fim do array
+
+const pushPop = ['a','b','c']
+console.log(pushPop)
+pushPop.push('d','e')
+console.log(pushPop)
+pushPop.pop()
+console.log(pushPop)
+const itemsPop = pushPop.pop()
+console.log(itemsPop)
+console.log(pushPop)
+
+// métodos de array:shift and unshift
+// shift remove o primeiro elemento do array
+// unshift adiciona itens no inicio do array
+
+const init = [1, 2, 3, 4, 5]
+console.log(init)
+
+const remoção = init.shift()
+init.unshift(6)
+
+console.log(init)
+
+// métodos do array:indexOf and lastIndexOf
+// o indexOf serve para identificar o indice de um elemento ou seja a posição
+// lastIndexOf usado para identificar o indíce da ultima ocorrencia de um elemento duplicado
+// quando um elemento do array não é encontrado, é retornado -1, fora da  caixa de index ou seja talvez não exista.
+const string = 'baralho'
+console.log(string.indexOf('o'))
+console.log(string.lastIndexOf('a'))
+
+const frutas = ['banana', 'maçã', 'laranja', 'tangerina', 'banana', 'uva']
+console.log(frutas.indexOf('laranja'))
+console.log(frutas.lastIndexOf('banana'))
+console.log(`${frutas[frutas.lastIndexOf('banana')]}, se encontra no indice [${frutas.lastIndexOf('banana')}]`)
+
+// método do array:include
+// include retorna um valor do tipo boolean, se o elemento existir retornará true
+console.log(`tangerina se encontra no array? ${frutas.includes('tangerina')}. está no indice [${frutas.indexOf('tangerina')}]`)
+
+// métod array: slice
+// utilizado para extrair um array menor de um array maior
+// o ultimo elemento é ignorado, ele vai até o penultimo por padrão
+// por exemplo se eu definir o fim como indice 4, ele vai pegar só os indices até 3
+// se eu só colocar o indice de start ele vai pegar todos elementos apartir do indice de start
+const arrayMaior = ['1', '2', '3', '4', '5', '6']
+console.log(arrayMaior)
+const arrayMenor = arrayMaior.slice(0, 4)
+console.log(arrayMenor)
+console.log(arrayMaior)
+const arrayApartir = arrayMaior.slice(2)
+console.log(arrayApartir)
+
+// método do array: forEach
+// ele percorre cada um dos elementos de um array
+// bom para percorrer todo o array
+
+const numeros= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numeros.forEach((numeros) => {
+    console.log(`percorrendo o numero ${numeros} com forEach`)
+})
+
+const letras = ['a', 'b', 'c', 'd', 'f', 'g']
+letras.forEach((letra) => {
+    console.log(`alfabeto, letra: ${letra}`)
+})
+
+const bdSimulado = [
+    { linguagem: 'java script', category: 'front-end'},
+    { linguagem: 'html', category: 'front-end'},
+    { linguagem: 'css', category: 'front-end'},
+    { linguagem: 'c#', category: 'back-end'},
+    { linguagem: 'c++', category: 'back-end'}
+]
+
+bdSimulado.forEach((chamaBd) => {
+    console.log(`linguagem = ${chamaBd.linguagem}, categoria = ${chamaBd.category}`)
+})
+
+// método do array: reverse
+// ele modifica o array original
+// inverte a ordem dos array
+
+const normal = [1, 2, 3, 4, 5, 6]
+console.log(normal)
+const inverso = normal.reverse()
+console.log(`array inverso ${inverso}`)
+console.log(`array normal afetado pela ref${normal}`) // ele foi invertido pq foi afetado pela referencia
