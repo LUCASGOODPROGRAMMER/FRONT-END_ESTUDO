0a792bdc540bb680205bdbd35567863f2b6488f9
LUCASGOODPROGRAMMER <lucashubner2019@gmaiil.com>
2025-07-22 08:21:05 -0400

    public acesso

---
+++ html_css_js_react/3_MAIS_SOBRE_REACT/avancando_em_react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './assets'
+
+function App() {
+
+  return (
+    <>
+      <h1>avançando em react</h1>
+      {/**acessandp imagem em public*/}
+      <img src="/Guts.jpg" alt="Guts" /> {/**o / vai se comportar como se fosse a raiz do public */}
+      
+    </>
+  )
+}
+
+export default App
