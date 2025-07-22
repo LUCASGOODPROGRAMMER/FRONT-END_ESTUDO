2a86160cb6826f5702e1cca2e82a00378e5f68ee
LUCASGOODPROGRAMMER <lucashubner2019@gmaiil.com>
2025-07-22 10:06:20 -0400

    mais sobre key

--- html_css_js_react/3_MAIS_SOBRE_REACT/avancando_em_react/src/components/ListRender.jsx
+++ html_css_js_react/3_MAIS_SOBRE_REACT/avancando_em_react/src/components/ListRender.jsx
@@ -1,17 +1,32 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 const ListRender = () => {
+  const [list] = useState(["Lucas", "Keynner", `Einstein`, "Lucas"]);
 
-    const [list] = useState(["Lucas", 'Keynner', `Einstein`])
+  const [users, serUsers] = useState([
+    { id: 1, name: "lucas", age: 17 },
+    { id: 2, name: "keynner", age: 18 },
+    { id: 3, name: "manu", age: 17 }
+  ]);
+  return (
+    <>
+      {/**render sem key */}
+      <ul>
+        {list.map((item, i) => (
+          <li /**key={i}*/>{item}</li>
+        ))}
+      </ul>
 
+      {/**render comm key*/}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age} anos
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-    return(
-        <ul>
-            {list.map((item) => (
-                <li>{item}</li>
-            ))}
-        </ul>
-    )
-}
-
-export default ListRender
+export default ListRender;
