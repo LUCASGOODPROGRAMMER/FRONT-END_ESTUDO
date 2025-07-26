269bfae60bfcf13d72f298db69f707b5a4ef464c
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-07-26 10:25:59 -0400

    inline style dinâmico

--- html_css_js_react/4_CSS_REACT/css_react/src/App.jsx
+++ html_css_js_react/4_CSS_REACT/css_react/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 /** css de component */
-import MyComponent from './components/MyComponent'
+import MyComponent from "./components/MyComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // css inline dinâmico
+  const n = Math.round(Math.random() * 2);
+  /**
+   * Math.floor(4.7)     // 4 - Arredonda para baixo
+   * Math.ceil(4.1)      // 5 - Arredonda para cima
+   * Math.round(4.5)     // 5 - Arredonda para o inteiro mais próximo
+   * Math.trunc(4.9)     // 4 - Remove a parte decimal 
+   * Math.min(5, 1, 9)   // 1 - Menor número
+   * Math.max(5, 1, 9)   // 9 - Maior número
+   * Math.pow(2, 3)      // 8 - 2 elevado a 3
+   * Math.sqrt(9)        // 3 - Raiz quadrada de 9
+   * Math.abs(-7)        // 7 - Retorna o valor positivo
+   * Math.random();  // ex: 0.43824234
+   * */
 
   return (
     <>
@@ -14,14 +27,31 @@ function App() {
         <h1>css no react</h1>
 
         {/** css de component */}
-        <MyComponent/>
+        <MyComponent />
         <p>parágrafo afetado pelo css do componente</p>
 
         {/** inline style (prática inadequada para manutenção e legibilidade do código)*/}
-        <p style={{color: "purple", fontWeight:"bold", backgroundColor:"black", borderRadius:"20px", placeSelf:"center", padding:"10px", margin:"10px"}}>Este elemento tem estilos inline</p>
+        <p
+          style={{
+            color: "purple",
+            fontWeight: "bold",
+            backgroundColor: "black",
+            borderRadius: "20px",
+            placeSelf: "center",
+            padding: "10px",
+            margin: "10px",
+          }}
+        >
+          Este elemento tem estilos inline
+        </p>
+
+        {/** inline style dinâmico */}
+        <h2 style={n > 1 ? { color: "magenta" } : { color: "purple" }}>
+          CSS dinâmico
+        </h2>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
