ad4244dd0bc7722a029ef5b7adcdffa5c36b470f
LUCASGOODPROGRAMMER <lucashubner2019@gmaiil.com>
2025-08-02 11:42:47 -0400

    NavLink

--- html_css_js_react/7_react_router/router/src/components/NavBar.jsx
+++ html_css_js_react/7_react_router/router/src/components/NavBar.jsx
@@ -1,14 +1,25 @@
 // usando Link um componente do react router dom usado para navegação entre páginas
 // Link é uma substituição do <a href="...", diferen do <a> ele não causa recarregamento de página
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
+
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link> 
-        <Link to="contact">Contact</Link>
+      {/* <Link to="/">Home</Link> 
+        <Link to="contact">Contact</Link> */}
+      <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+        Home
+      </NavLink>
+      <NavLink
+        to="/contact"
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
+        Contatos
+      </NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
